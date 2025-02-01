4cda3de14fb3e53e3d7ef95d09099130896f0d61
Vahedin1 <hamidovicvahedin1@gmail.com>
2025-02-01 18:59:41 +0100

    Attempt 8

--- src/pages/Unger.jsx
+++ src/pages/Unger.jsx
@@ -1,17 +1,14 @@
 import React from "react";
 import { Container } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
-import HeaderMenuButton from "../components/Project-Header.jsx";
 
 const Unger = () => {
   const theme = useTheme();
   return (
     <div>
-      <HeaderMenuButton />
       <Container
         disableGutters
         sx={{
-          backgroundColor: theme.palette.background.default, // Applying theme background color
         }}
       >
         {/** All content is commented out for debugging purposes **/}
